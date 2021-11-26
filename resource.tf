5639c11eb535b531088eaf1a20637b08835241e7
Ojasvi05 <95043478+Ojasvi05@users.noreply.github.com>
2021-11-26 16:37:37 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,35 @@
+	         variable "image" {
+	          type = string
+	         default = "ami-0108d6a82a783b352"
+	        }
+	
+	        variable "hw" {
+	         default = "t2.micro"
+	        }
+	
+	        variable "name" {
+	          default = "module-created-server"
+	        }
+	
+	        variable "nw-name" {
+	          default = "ojasvi-net"
+	        }
+	
+	        resource "aws_instance" "server1" {
+	          ami           = var.image
+	          instance_type = var.hw
+	          key_name = "test"
+	          tags = {
+	            Name = var.name
+	            Env  = "Prod"
+	          }
+	        }
+	
+	        resource "aws_vpc" "main" {
+	          cidr_block       = "10.0.0.0/16"
+	          instance_tenancy = "default"
+	
+	          tags = {
+	            Name = var.nw-name
+	          }
+        }
